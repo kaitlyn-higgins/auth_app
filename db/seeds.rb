8c8b2d9f37e8c784697b18f5735a2cc83267f19e
kaitlyn_higgins <kaitlyn.brooke.higgins@gmail.com>
2019-05-22 22:53:29 -0500

    Seed dump users into seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {name: "Matilda Verde", email: "matilda@gmail.com", password_digest: "$2a$10$qw2Mh8.tol.GmEU7CLs9xeqADpdN07btXhaRadBHG/ne42YfgEXsW"},
+  {name: "Andrew Wilson", email: "andrew@gmail.com", password_digest: "$2a$10$bgepk3HrKAUsiQ1OUG1CvOywLEPOn9NVGiXSiiGSnQjyuenAC6zAu"},
+  {name: "Wilson James", email: "wilson@gmail.com", password_digest: "$2a$10$ZxzkcwmVop3FyXSHedAZB.7SX5LZNLX3gWYkYHiqw/pjgUrdzyy02"},
+  {name: "Lydia Croom", email: "lydia@gmail.com", password_digest: "$2a$10$W6XGPisr61oyN9DPeO97f.ia/e103zHCU0uHl4sGo7aD/OabccPzG"},
+  {name: "Ariel Grier", email: "ariel@gmail.com", password_digest: "$2a$10$HrezD/ZfQaED5xHXoJdWsutidVpP7KIuG2.45X23DhEGYdxf0VPdC"},
+  {name: "Abbey Lion", email: "abbey@gmail.com", password_digest: "$2a$10$40y0Dc6Hwb6JEyuaUMzZP.8E7xDrb0V3vxcULeqqA6wo42fZwSHtm"},
+  {name: "Zuriel Mason", email: "zuriel@gmail.com", password_digest: "$2a$10$nBGr1ymkLyzAeXKRgREsP.jpoknNlkjzc2Pi0PzZLY2JeuuVYz902"}
+])
